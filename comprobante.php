5f59b1ea5f55d6c4ca04261497e49f8621b1b1c1
AntiTriki <ne.i.on@hotmail.com>
2017-10-09 08:56:27 -0400

    compro
    wee

--- comprobante.php
+++ comprobante.php
@@ -1,14 +1,13 @@
 <?php
 include_once('conexion.php');
-<<<<<<< HEAD
+
 
 /*porsiaka creo q despues escoger periodo se va a comprobante
  * if(isset($_SESSION['id_ges']))
 {
     unset($_SESSION['id_ges']);
 }*/
-=======
->>>>>>> 97cf939147a1097f9cb4c6513c612f90b520637c
+
 include_once('bar.php');
 
 
@@ -27,63 +26,24 @@ date_default_timezone_set('America/La_Paz');
 
             color: #000000;
         }
-<<<<<<< HEAD
-        .table.table-hover.table-bordered tr:hover {
-            background: none;
-        }
-        td:hover {
-            background: lightgray;
-        }
-        /* a few selector examples */
-        td[colspan]:hover {
-            background: lime;
-        }
-        td[colspan] + td[colspan]:hover {
-            background:turquoise;
-        }
-        td[colspan="2"]:hover {
-            background: gold;
-        }
-        td[rowspan]:hover {
-            background: tomato;
-=======
+
+        
         .borde {
             border: 2px solid #a1a1a1;
             padding: 10px 40px;
             border-radius: 3px;
             width: 80%;
->>>>>>> 97cf939147a1097f9cb4c6513c612f90b520637c
+
         }
     </style>
   </head>
  <body>
-<<<<<<< HEAD
- <table class="table table-hover table-bordered">
-     <thead>
-     <tr>
-         <th class="danger" colspan="3">Comprobante de Pago</th>
-     </tr>
-     </thead>
-     <tr>
-         <td class="warning" width="10%">Fecha:</td>
-         <td colspan="2" id="vertical_align">sdf</td>
-     </tr>
-     <tr>
-         <td rowspan="3" class="asd">Client</td>
-         <td colspan="1">NAME</td>
-         <td colspan="1">NAME 2</td>
-     </tr>
-     <tr>
-         <td colspan="2">asd</td>
-     </tr>
-     <tr class="info">
-         <td colspan="2">asd</td>
-     </tr>
- </table>
+
+
 
 
  </body>
-=======
+
  <div class="container-fluid" style="margin-left: 250px">
      <div class="text-center">
          <h5>Comprobante</h5>
@@ -194,75 +154,7 @@ date_default_timezone_set('America/La_Paz');
          <button type="button" class="btn btn-danger"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
      </div>
      <div style="position: relative;" class="borde">
-         <form class="" role="form" method="post" action="productoregister.php" enctype="multipart/form-data">
-             <div class="container">
-                 <div class="row row-centered">
-                     <div class="col-sm-3 col-lg-3 col-centered">
-                         <!-- <div class="form-group">
-                 <label for="input6" class="col-md-4 control-label">123456789012:</label>
-                 <div class="col-md-8">
-                 <input type="text" class="form-control" id="input6" placeholder="input 6">
-               </div>
-             </div> -->
 
-                     </div>
-                 </div>
-             </div>
-             <div class="row">
-                 <div class="col-sm-2 col-lg-2">
-                     <div class="form-group">
-                         <label for="serie">Serie:</label>
-                         <input type="text" class="form-control" id="serie">
-                     </div>
-                 </div>
-                 <div class="col-sm-2 col-lg-6">
-                     <div class="form-group">
-                         <label for="tipo_comprobante">Tipo de Comprobante:</label>
-                         <input type="text" class="form-control" id="tipo_comprobante">
-                     </div>
-                 </div>
-                 <div class="col-sm-2 col-lg-4">
-                     <div class="form-group">
-                         <label for="fecha">Fecha:</label>
-                         <input type="text" class="form-control" id="fecha">
-                     </div>
-                 </div>
-             </div>
-             <div class="row">
-                 <div class="col-sm-2 col-lg-12">
-                     <div class="form-group">
-                         <label for="glosa">Glosa:</label>
-                         <input type="text" class="form-control" id="glosa">
-                     </div>
-                 </div>
-
-             </div>
-             <div class="row">
-
-                 <div class="col-sm-2 col-lg-3">
-                     <div class="form-group">
-                         <label for="tipo_cambio">Tipo de Cambio:</label>
-                         <input type="text" class="form-control" id="tipo_cambio">
-                     </div>
-                 </div>
-                 <div class="col-sm-2 col-lg-5">
-                     <div class="form-group">
-                         <label for="moneda">Moneda:</label>
-                         <input type="text" class="form-control" id="moneda">
-                     </div>
-                 </div>
-                 <div class="col-sm-2 col-lg-4">
-                     <div class="form-group">
-                         <label for="estado">Estado:</label>
-                         <input type="text" class="form-control" id="estado">
-                     </div>
-                 </div>
-
-             </div>
-
-             <!-- /.row this actually does not appear to be needed with the form-horizontal -->
-
-         </form>
 
          <div class="row">
              <div class='wrapper text-center'>
@@ -302,5 +194,5 @@ date_default_timezone_set('America/La_Paz');
              })
          });
      </script></body>
->>>>>>> 97cf939147a1097f9cb4c6513c612f90b520637c
+
 </html>
