34da054da69a3ad730b8de444fc5ced1ac05b1e0
AntiTriki <ne.i.on@hotmail.com>
2017-10-30 08:24:10 -0400

    antes de irme
    ya

--- gestiones.php
+++ gestiones.php
@@ -22,7 +22,7 @@ try{
        $ffin = date('Y-m-d', strtotime($ffin));
        $result = mysql_query("SELECT COUNT(*) AS conteo FROM gestion where id_empresa=".$_SESSION['id_emp']." and estado=1 ;");
        $row = mysql_fetch_array($result);
-       if ($row['conteo'] < 1){
+       if ($row['conteo'] < 3){
         $result = mysql_query("SELECT COUNT(*) AS conteo FROM gestion where id_empresa=".$_SESSION['id_emp']." and nombre='".$_POST["nombre"]."' ;");
         $row = mysql_fetch_array($result);
           if ($row['conteo'] < 3){
@@ -67,17 +67,63 @@ try{
           print json_encode($jTableResult);
         }
     }else if($_GET["accion"] == "actualizar"){
-      $finicio = str_replace('/', '-', $_POST["fecha_inicio"]);
-       $finicio = date('Y-m-d', strtotime($finicio));
-       $ffin = str_replace('/', '-', $_POST["fecha_fin"]);
-       $ffin = date('Y-m-d', strtotime($ffin));
+
 
        //validaciones tipo crear.... 2 gestiones abiertas mismo nombre dentro de la gestion etc
+        //Se podrá modificar (fechas) siempre y cuando no tenga periodos
+        //Se podrá eliminar de la BD siempre que no este relacionado
+        //No se pueden modificar los datos de una gestión cerrada
+        //No pueden existir mas 2 gestion abiertas
+        //No pueden Existir 2 gestiones con el mismo nombre para la misma empresa
+
+        $finicio = str_replace('/', '-', $_POST["fecha_inicio"]);
+        $finicio = date('Y-m-d', strtotime($finicio));
+        $ffin = str_replace('/', '-', $_POST["fecha_fin"]);
+        $ffin = date('Y-m-d', strtotime($ffin));
+        $result = mysql_query("SELECT COUNT(*) AS conteo FROM gestion where id_empresa=".$_SESSION['id_emp']." and estado=1 ;");
+        $row = mysql_fetch_array($result);
+        if ($row['conteo'] < 3){
+            $result = mysql_query("SELECT COUNT(*) AS conteo FROM gestion where id_empresa=".$_SESSION['id_emp']." and nombre='".$_POST["nombre"]."' ;");
+            $row = mysql_fetch_array($result);
+            if ($row['conteo'] < 3){
+                if($finicio<$ffin){
+                    $result = mysql_query("SELECT COUNT(*) AS cont FROM gestion where id_empresa=".$_SESSION['id_emp']." and ((fecha_inicio between '".$finicio."' and '".$ffin."') or (fecha_fin between '".$finicio."' and '".$ffin."')) ;");
+                    $row = mysql_fetch_array($result);
+                    if($row['cont'] == 0){
         $result = mysql_query("UPDATE gestion SET nombre='".$_POST["nombre"]."', fecha_inicio='$finicio', fecha_fin='$ffin',
 		 estado=".$_POST["estado"]." WHERE id=" . $_POST["id"] . ";");
         $jTableResult = array();
         $jTableResult['Result'] = "OK";
         print json_encode($jTableResult);
+                }else{
+                    $row = mysql_fetch_array($result);
+                    $jTableResult = array();
+                    $jTableResult['Result'] = "ERROR";
+                    $jTableResult['Message'] = "Se solapan las fechas de creacion con otras registradas";
+                    print json_encode($jTableResult);
+                }
+            }else{
+                $row = mysql_fetch_array($result);
+                $jTableResult = array();
+                $jTableResult['Result'] = "ERROR";
+                $jTableResult['Message'] = "La fecha de inicio debe ser menor que la fecha final";
+                print json_encode($jTableResult);
+            }
+            }else{
+                $row = mysql_fetch_array($result);
+                $jTableResult = array();
+                $jTableResult['Result'] = "ERROR";
+                $jTableResult['Message'] = "Ya existe una gestion con el mismo nombre";
+                print json_encode($jTableResult);
+            }
+    }else {
+        $row = mysql_fetch_array($result);
+        $jTableResult = array();
+        $jTableResult['Result'] = "ERROR";
+        $jTableResult['Message'] = "No puede tener 3 gestiones abiertas";
+        print json_encode($jTableResult);
+    }
+
     }else if($_GET["accion"] == "eliminar"){
         $result = mysql_query("DELETE FROM gestion WHERE id= " . $_POST["id"] . ";");
         $jTableResult = array();
