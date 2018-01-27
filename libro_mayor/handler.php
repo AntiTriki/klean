08589c9c357f69d3e811b0472ba6eb1dce870727
AntiTriki <ne.i.on@hotmail.com>
2018-01-27 13:01:29 -0400

    gfdgfd
    fdgfdgfdgdfgfdg

---
+++ libro_mayor/handler.php
@@ -0,0 +1,25 @@
+<?php
+session_name('nilds');
+session_start();
+require_once "stimulsoft/helper.php";
+
+// Please configure the security level as you required.
+// By default is to allow any requests from any domains.
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Engaged-Auth-Token");
+
+$handler = new StiHandler();
+$handler->registerErrorHandlers();
+
+$handler->onBeginProcessData = function ($args) {
+	$database = $args->database;
+    $connectionString = $args->connectionString;
+    $queryString = $args->queryString;
+    $args->parameters["var"] = $_SESSION['id_emp'];
+    $args->parameters["per"] = $_SESSION['id_per'];
+	$args->parameters["usu"] = $_SESSION['id_usuario'];
+	return StiResult::success();
+	
+};
+
+$handler->process();
